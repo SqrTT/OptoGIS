174f804fcb95d7338259732430207042bdfca7d7
SqrTT <tolik_st@inbox.ru>
2012-11-09 01:15:03 +0200

    add new models

---
+++ gis/protected/models/Invent.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This is the model class for table "invent".
+ *
+ * The followings are the available columns in table 'invent':
+ * @property integer $id
+ * @property integer $node_id
+ * @property integer $type
+ * @property integer $parent
+ * @property string $SN
+ * @property string $des
+ */
+class Invent extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Invent the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'invent';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('node_id, type, parent, SN, des', 'required'),
+			array('node_id, type, parent', 'numerical', 'integerOnly'=>true),
+			array('SN', 'length', 'max'=>50),
+			array('des', 'length', 'max'=>150),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, node_id, type, parent, SN, des', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'node_id' => 'Node',
+			'type' => 'Type',
+			'parent' => 'Parent',
+			'SN' => 'Sn',
+			'des' => 'Des',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('node_id',$this->node_id);
+		$criteria->compare('type',$this->type);
+		$criteria->compare('parent',$this->parent);
+		$criteria->compare('SN',$this->SN,true);
+		$criteria->compare('des',$this->des,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
---
+++ gis/protected/models/Lines.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This is the model class for table "lines".
+ *
+ * The followings are the available columns in table 'lines':
+ * @property integer $line_id
+ * @property integer $frm_pt_id
+ * @property integer $to_pt_id
+ * @property integer $frm_inv
+ * @property integer $to_inv
+ * @property integer $type_line_id
+ * @property integer $lenght
+ * @property integer $clients
+ * @property integer $who
+ * @property string $when
+ */
+class Lines extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Lines the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'lines';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('frm_pt_id, to_pt_id, frm_inv, to_inv, type_line_id, lenght, who, when', 'required'),
+			array('frm_pt_id, to_pt_id, frm_inv, to_inv, type_line_id, lenght, clients, who', 'numerical', 'integerOnly'=>true),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('line_id, frm_pt_id, to_pt_id, frm_inv, to_inv, type_line_id, lenght, clients, who, when', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'line_id' => 'Line',
+			'frm_pt_id' => 'Frm Pt',
+			'to_pt_id' => 'To Pt',
+			'frm_inv' => 'Frm Inv',
+			'to_inv' => 'To Inv',
+			'type_line_id' => 'Type Line',
+			'lenght' => 'Lenght',
+			'clients' => 'Clients',
+			'who' => 'Who',
+			'when' => 'When',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('line_id',$this->line_id);
+		$criteria->compare('frm_pt_id',$this->frm_pt_id);
+		$criteria->compare('to_pt_id',$this->to_pt_id);
+		$criteria->compare('frm_inv',$this->frm_inv);
+		$criteria->compare('to_inv',$this->to_inv);
+		$criteria->compare('type_line_id',$this->type_line_id);
+		$criteria->compare('lenght',$this->lenght);
+		$criteria->compare('clients',$this->clients);
+		$criteria->compare('who',$this->who);
+		$criteria->compare('when',$this->when,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
